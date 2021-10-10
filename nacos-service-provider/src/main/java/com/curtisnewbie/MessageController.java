2dd6764ab75e00554c6ab9fef9d997c0eb8956fc
CurtisNewbie <zhuangyongj@gmail.com>
2021-10-10 23:35:21 +0800

    Demo for config-center using nacos

--- nacos-service-provider/src/main/java/com/curtisnewbie/MessageController.java
+++ nacos-service-provider/src/main/java/com/curtisnewbie/MessageController.java
@@ -1,5 +1,7 @@
 package com.curtisnewbie;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,23 @@ import java.util.Date;
 /**
  * @author yongjie.zhuang
  */
+@RefreshScope
 @RestController
 @RequestMapping("/messages")
 public class MessageController {
+
+    @Value("${config:no config}")
+    private String config;
 
     @GetMapping("/latest")
     public String getLatestMessage() {
         return "Now " + new Date().toString();
     }
+
+    // demo for config-center
+    @GetMapping("/config")
+    public String getConfig(){
+        return config;
+    }
+
 }
